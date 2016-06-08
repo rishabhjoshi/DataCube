15e50c904ef18d8fdaf916ef9ff0fbdf38b9f95b
Rishabh Joshi <rj04796@live.com>
2016-06-08 10:27:12 +0530

    added cache info for speed optimization

--- DataConversion/tifwarp.py
+++ DataConversion/tifwarp.py
@@ -11,4 +11,7 @@ out = files[0].split('.')[0]
 out = out[:-6] + "_warped.tif"
 
 cmd = "gdalwarp -of GTiff " + inp + " " + out
+# use "gdalwarp -of GTiff --config GDAL_CACHEMAX 3000 -wm 3000 in out
+# This sets the cache of for gdalwarp. Dont define cache more than RAM
+# 3000 signifies 3Gigs of RAM
 subprocess.call(cmd, shell = True)
